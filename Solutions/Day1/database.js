192765e594d9250298f3e4eb9ee3dac212e888f1
raj23689 <92665411+raj23689@users.noreply.github.com>
2022-07-01 22:42:59 +0530

    chore: added databse.js and lore.js and completed the Day 1 exercises

---
+++ Solutions/Day1/database.js
@@ -0,0 +1,13 @@
+let firstChoice = 'Yes'
+
+let isMarried = true
+
+let lastName
+
+let middleName = null
+
+
+console.log(typeof firstChoice)
+console.log(typeof isMarried)
+console.log(typeof lastName)
+console.log(typeof middleName)
---
+++ Solutions/Day1/lore.js
@@ -0,0 +1,28 @@
+// For questions from 6 - 10 of Day 1 Exercise
+
+let mine
+let isSingle
+let marriedWoman
+let watchedAnime
+
+// assigned value
+
+let ju_space = 10
+const isAnime = true
+const isCartoon = false
+let biggestPlanet = "Jupiter"
+
+let firstName = "Raj"
+let lastName = "Majumdar"
+let maritalStatus = "Single"
+let country = "Japan"
+let age = 100
+
+let fName = "Raj", lName = "Majumdar", mStatus = "Single", cntry = "Japan", ae = 100
+
+
+let myAge = 25
+let yourAge = 30
+
+console.log("I am", + myAge, "yrs old")
+console.log("You are", + yourAge, "yrs old")
